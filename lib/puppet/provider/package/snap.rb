631e6fd5ddde4650adac87eeb4e3ef09b37ad22d
Amir Yalon <git@please.nospammail.net>
2020-01-15 16:56:15 +0200

    Treat snap command output as UTF-8

--- lib/puppet/provider/package/snap.rb
+++ lib/puppet/provider/package/snap.rb
@@ -12,7 +12,7 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
 
   def self.instances
     output = installer "list"
-    lines = output.split("\n")
+    lines = output.force_encoding("UTF-8").split("\n")
     lines.shift # skip header
     instances = []
 
@@ -59,7 +59,7 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
 
   def latest
     output = installer "search", @resource[:name]
-    lines = output.split("\n")
+    lines = output.force_encoding("UTF-8").split("\n")
     lines.shift # skip header
 
     lines.each { |line|
